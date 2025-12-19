a6ab4825cb276162a2032ef36193ff69bdb475ed
HaiNgo276 <ngohai270603@gmail.com>
2025-12-20 01:24:32 +0700

    update

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import svgr from 'vite-plugin-svgr'
+import envCompatible from 'vite-plugin-env-compatible'
+import topLevelAwait from 'vite-plugin-top-level-await'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr(),
+    envCompatible(),
+    topLevelAwait()
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'antd-vendor': ['antd', '@ant-design/icons'],
+          'utils': ['axios', 'zustand']
+        }
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    host: true
+  }
+})
